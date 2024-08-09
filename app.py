3c1c5edeeb47339bf106ca4b9a1d85308cdca216
cabralotavio3 <bugvug250@gmail.com>
2024-08-09 10:35:41 -0300

    new file:   static/css/estilo.css 	new file:   static/img/alice_unpluged.webp 	new file:   static/img/teto_pou.jpeg     new file:   templates/aula.html 	new file:   templates/index.html

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Olá mundo flask'
+    return render_template('index.html')
+
+@app.route('/aula/')
+@app.route('/aula/<nome>')
+@app.route('/aula/<nome>/<curso>/<int:ano_nasc>')
+def aula(nome = 'Otávio Cabral', curso = '2°informatica', ano_nasc = 2007):
+    dados = {'nome' : nome, 'curso' : curso, 'ano_nasc' : ano_nasc }
+    return render_template('aula.html',dados_html =dados)
 
 if __name__ == '__main__':
     app.run()
