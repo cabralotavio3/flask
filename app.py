be2ecb493f85ad92e529b028fb86f0bcf6f87f06
cabralotavio3 <bugvug250@gmail.com>
2024-08-23 10:31:42 -0300

    modified:   __pycache__/app.cpython-310.pyc 	modified:   app.py 	new file:   database.py 	new file:   migrations/README 	new file:   migrations/alembic.ini 	new file:   migrations/env.py     new file:   migrations/script.py.mako 	new file:   migrations/versions/bc87afd98378_create_table_usuario.py 	new file:   models.py 	modified:   templates/base.html 	modified:   templates/dados.html 	modified:   templates/form.html 	new file:   templates/usuario_lista.html

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template, request
+from database import db
+from flask_migrate import Migrate
+from models import Usuario
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'you-will-never-guess'
+
+#drive://usuario:senha@servidor/banco_dados
+conexao = "mysql+pymysql://alunos:cefetmg@127.0.0.1/flasko"
+app.config['SQLALCHEMY_DATABASE_URI'] = conexao
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
@@ -21,6 +33,11 @@ def form():
 def dados():
     dados = request.form
     return render_template('dados.html', dados_html = dados)
+
+@app.route('/usuario')
+def usuario():
+    u = Usuario.query.all()
+    return render_template('usuario_lista.html', dados_html = u)
 
 @app.route
 def base():
