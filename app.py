58a74e5a6665034075a7534aa3181df637d43ee3
cabralotavio3 <bugvug250@gmail.com>
2024-08-30 10:34:42 -0300

    modified:   __pycache__/app.cpython-310.pyc 	modified:   app.py 	modified:   static/css/estilo.css 	modified:   templates/base.html 	modified:   templates/dados.html 	new file:   templates/usuario_add.html 	modified:   templates/usuario_lista.html

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template, request
+#pip install flask
+#pip install Flask-SQLAlchemy
+#pip install Flask-Migrate
+#pip install Flask-Script
+#pip install pymysql
+#flask db init
+#flask db migrate -m "Migracao inicial"
+#flask db upgrade
+from flask import Flask, render_template, request, flash, redirect
 from database import db
 from flask_migrate import Migrate
 from models import Usuario
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'you-will-never-guess'
+app.config['SECRET_KEY'] = '123'
 
 #drive://usuario:senha@servidor/banco_dados
 conexao = "mysql+pymysql://alunos:cefetmg@127.0.0.1/flasko"
@@ -39,6 +47,37 @@ def usuario():
     u = Usuario.query.all()
     return render_template('usuario_lista.html', dados_html = u)
 
+@app.route('/usuario/add')
+def usuario_add():
+    u = Usuario.query.all()
+    return render_template('usuario_add.html', dados_html = u)
+
+@app.route('/usuario/save', methods=['POST'])
+def usuario_save():
+    nome = request.form.get('nome')
+    email = request.form.get('email')
+    idade = request.form.get('idade')
+    if nome and email and idade:
+        usuario = Usuario(nome, email, idade)
+        db.session.add(usuario)
+        db.session.commit()
+        flash('usuario cadastrado com sucesso fera!!')
+        return redirect('/usuario')
+    else:
+        flash('preencha todos os campos zeca')
+        return redirect('/usuario/add')
+
+@app.route('/usuario/remove/<int:id>')
+def usuario_remove(id):
+    if id > 0:
+        usuario = Usuario.query.get(id)
+        db.session.remove(usuario)
+        db.session.commit()
+        flash('Foi Jubilado Com sucesso!!!!!!!')
+        return redirect('/usuario')
+    else:
+        flash('Deu certo nao irmao faz o L')
+        return redirect('/usuario')
 @app.route
 def base():
     return render_template('base.html')
